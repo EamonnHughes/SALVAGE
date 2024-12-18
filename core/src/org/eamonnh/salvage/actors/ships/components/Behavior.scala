ec5ac2986247757e8c476d592289c71e38bf1820
Eamonn Hughes <efohughes@gmail.com>
2024-12-18 11:56:58 -0500

    wokring turning

--- core/src/org/eamonnh/salvage/actors/ships/components/Behavior.scala
+++ core/src/org/eamonnh/salvage/actors/ships/components/Behavior.scala
@@ -10,11 +10,17 @@ abstract class Behavior {
 
 class Convoy(target: Ship) extends Behavior {
   override def update(ship: Ship, game: Game): Unit = {
-
-    var targetRot = (Math.atan2(target.location.y - ship.location.y, target.location.x - ship.location.x) + (Math.PI * 2)) % (Math.PI*2)
-    var otherTarget = targetRot - Math.PI * 2
-    if(Math.abs(ship.rotation - targetRot) > (Math.PI / 8)) {
-      if((targetRot - ship.rotation) <= (otherTarget - ship.rotation)) {
+    def targetRot = {
+      if (ship.location.distanceFrom(target.location) < 5) {
+        target.rotation
+      } else {
+        (Math.atan2(target.location.y - ship.location.y, target.location.x - ship.location.x) + (Math.PI * 2)) % (Math.PI * 2)
+      }
+    }
+    var t1 = (ship.rotation - targetRot + (Math.PI * 2)) % (Math.PI*2)
+    var t2 = (targetRot - ship.rotation + (Math.PI * 2)) % (Math.PI*2)
+    if(Math.min(t1, t2) > (Math.PI / 16)) {
+      if(t1 < t2) {
         ship.rotatingRight = true
         ship.rotatingLeft = false
       } else {
@@ -25,8 +31,8 @@ class Convoy(target: Ship) extends Behavior {
       ship.rotatingRight = false
       ship.rotatingLeft = false
     }
-     if(Math.abs(ship.rotation - targetRot) < (Math.PI / 4)) {
-      if(ship.location.distanceFrom(target.location) > 25) {
+     if(Math.min(t1, t2) < (Math.PI / 4)) {
+      if(ship.location.distanceFrom(target.location) > 15) {
         ship.movingFullSpeed = true
         ship.movingSlower = false
       } else if (ship.location.distanceFrom(target.location) > 5) {
